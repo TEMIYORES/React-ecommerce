cad73d18919c2da792c6771a8ce718b4e42b92c2
TEMIYORES <temiyores1234@gmail.com>
2021-12-18 14:01:46 +0000

    Navbar completed

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    font-weight: 500;
+    font-size: 14px;
+`
+function Announcement() {
+    return (
+        <Container>
+            Super Deal!, Free shipping over 50%
+        </Container>
+    )
+}
+
+export default Announcement
---
+++ src/components/Slider.jsx
@@ -0,0 +1,61 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  opacity: 0.6;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+`;
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img``;
+const InfoContainer = styled.div`
+  flex: 1;
+`;
+function Slider() {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="background-1.png" />
+          </ImgContainer>
+          <InfoContainer></InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+}
+
+export default Slider;
